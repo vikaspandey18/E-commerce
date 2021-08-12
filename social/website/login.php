c4706335e74691ab88370900fa0554788382c9ce
Vikas Pandey <86400964+vikaspandey18@users.noreply.github.com>
2021-08-12 16:41:44 +0530

    Changing the text of login page

--- social/website/login.php
+++ social/website/login.php
@@ -97,11 +97,6 @@ if (isset($_POST['save'])) {
                         <!-- /.col -->
                     </div>
                 </form>
-
-
-                <p class="mb-1">
-                    <a href="forgot-password.html">I forgot my password</a>
-                </p>
                 <p class="mb-0">
                     <a href="register.php" class="text-center">Register a new membership</a>
                 </p>
