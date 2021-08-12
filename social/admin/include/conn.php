46850593bd0dfdc45637080ac7cd14e2dc2434cb
Vikas Pandey <86400964+vikaspandey18@users.noreply.github.com>
2021-08-12 17:05:18 +0530

    Changing local database

--- social/admin/include/conn.php
+++ social/admin/include/conn.php
@@ -1,4 +1,10 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'root', '', 'social') or mysqli_connect_errno($conn);
+//use for localhost database
+// $conn = mysqli_connect('localhost', 'root', '', 'social') or mysqli_connect_errno($conn);
+
+
+//using for using online
+$conn = mysqli_connect('remotemysql.com', 'eSl51a5zG5', 'OmW2hrGt9q', 'eSl51a5zG5') or die("Unable To Connect To Database");
+
 ?>
